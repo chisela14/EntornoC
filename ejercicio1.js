aac9af8809a4eaafec38a62382c1ad1734f675f3
Estudiante <chisela14@gmail.com>
2022-09-16 10:28:11 +0200

    first commit

---
+++ ejercicio1.js
@@ -0,0 +1,11 @@
+let nombres = []
+function inicializar(){
+    let nombre = prompt("Introduce un nombre");
+    if (nombres.indexOf(nombre) = -1)
+        nombres.push(nombre);
+    else
+        //mostrar alerta de que el nombre ya está en el array
+        
+    //devolver primera letra en mayúscula de cada uno
+    //de los nombres introducidos en el array ordenadas por orden alfabético.
+}
