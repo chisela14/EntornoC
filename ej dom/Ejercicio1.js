d0c08e68c2ccfba583098bbfc70429045c2c09a6
chisela14 <chisela14@gmail.com>
2022-09-21 14:28:09 +0200

    inicio ej 1

---
+++ ej dom/Ejercicio1.js
@@ -0,0 +1,7 @@
+// Número de enlaces de la página
+let href = document.querySelectorAll([href]);
+let src = document.querySelectorAll([src]);
+let numEnlaces = href.length + src.length;
+// Dirección a la que enlaza el penúltimo enlace
+// Numero de enlaces que enlazan a http://prueba
+// Número de enlaces del tercer párrafo
