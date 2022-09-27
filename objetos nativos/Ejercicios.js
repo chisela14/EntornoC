9609c3b4323ae6a12ea3b6fdc2bb68c1d794b7bb
chisela14 <chisela14@gmail.com>
2022-09-27 14:28:50 +0200

    27-9 clase

--- objetos nativos/Ejercicios.js
+++ objetos nativos/Ejercicios.js
@@ -4,14 +4,25 @@ window.moveBy(20,30);
 window.resizeTo(250,250);
 window.resizeBy(0,50);
 window.moveTo(0,0);
+
 // Actividad 2: Realiza algún ejemplo sencillo de acceso y modificación de los diferentes atributos 
 // del objeto document. Crea un documento html que contenga algún formulario, imágenes, anclas y enlaces y 
 // comprueba el contenido de los arrays que contienen información sobre dichos elementos en el objeto document. 
-document.title="Cambiar título";
-document.URL="http://nueva_pagina";
+document.title="Título cambiado";
+document.URL="http://nueva_pagina"; 
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
 // Actividad 3: Realiza un ejemplo donde obtengas toda la información posible del objeto location de forma ordenada. 
 // Utiliza los métodos del objeto location para modificar la url, comprobando la diferencia entre assign y replace, 
 // haz uso también del método reload.
+document.querySelector("#hash").innerHTML+=location.hash;
+document.querySelector("#host").innerHTML+=location.host;
+document.querySelector("#hostname").innerHTML+=location.hostname;
+document.querySelector("#href").innerHTML+=location.href;
+document.querySelector("#pathname").innerHTML+=location.pathname;
+document.querySelector("#port").innerHTML+=location.port;
+document.querySelector("#protocol").innerHTML+=location.protocol;
+document.querySelector("#search").innerHTML+=location.search;
 
 // Actividad 4: A partir de la página proporcionada en las transparencias 
 // https://developer.mozilla.org/es/docs/Web/API/Navigator indica y prueba algunos métodos útiles del objeto navigator.
