af234848c293e6b829b222fecdbf57299a5f57de
chisela14 <chisela14@gmail.com>
2022-09-29 14:28:52 +0200

    ej 2 acabado

--- fechas y timers/script.js
+++ fechas y timers/script.js
@@ -3,14 +3,14 @@
     // Hoy es martes, 28 de Febrero de 2018 y son las 14:32 horas.
     //https://gomakethings.com/how-to-get-and-set-a-date-object-from-an-input-with-vanilla-javascript/
 
-const weekday = ["domingo","lunes","martes","miércoles","jueves","viernes","sábado"];
-const month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+const weekdays = ["domingo","lunes","martes","miércoles","jueves","viernes","sábado"];
+const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-let dateField= document.querySelector("#date");
+let dateField= document.querySelector("#dateTime");
 dateField.addEventListener('input', mostrarFecha);
 function mostrarFecha(){
     let date = new Date (dateField.value);
-    console.log(`Has seleccionado el ${weekday[date.getDay()]} ${date.getDate()} de ${month[date.getMonth()]} de ${date.getFullYear()} a las ${date.getHours()}:${date.getMinutes()} horas.`);
+    console.log(`Has seleccionado el ${weekdays[date.getDay()]} ${date.getDate()} de ${months[date.getMonth()]} de ${date.getFullYear()} a las ${date.getHours()}:${date.getMinutes()} horas.`);
 }
 
 // 2. A continuación dos campos para introducir un mes y un año e imprima su calendario. 
@@ -18,10 +18,33 @@ function mostrarFecha(){
 // OCTUBRE – 2014
 // 1 (miercoles), 2 (jueves), ….. , 31 (viernes).
 
+let calendarButton = document.querySelector("#showCalendar");
+calendarButton.addEventListener('click', showCalendar);
 
+function showCalendar(){
+    let month = document.querySelector("#month").value-1;
+    let year = document.querySelector("#year").value;
+    let result = `${months[month].toUpperCase()} - ${year} <br>`;
+    let date = new Date(year, month);
+    let numberOfDays = new Date(year, month, 0).getDate();
+    let day = date.getDay();
+    for(let i = 1; i < numberOfDays; i++){
+        result += `${i} (${weekdays[day]}) <br>`;
+        if(day <6){
+            day++;  
+        }else{
+            day = 0;
+        }
+    }
+    document.getElementById("ejDos").innerHTML+= result;
+}
 
 // 3. Un campo que nos permita introducir una fecha y otro que nos permita introducir los días de retraso en el pago, 
 // una ves introducidos nos devolverá la fecha del pago.
+
+let delayDate = new Date (document.querySelector("#delayDate").value);
+let daysDelayed = document.querySelector("#daysDelayed");
+
 
 // 4. Un cronómetro con el siguiente formato:
 // Hoy es 30-9-2019 y son las 21:4:23 horas.
