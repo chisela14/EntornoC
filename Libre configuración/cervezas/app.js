1b86774f9265344c0a50da8a10307808d309031b
chisela14 <chisela14@gmail.com>
2022-10-24 12:22:56 +0200

    recurso bares hecho

--- Libre configuración/cervezas/app.js
+++ Libre configuración/cervezas/app.js
@@ -1,28 +1,18 @@
+//To DO: crear recurso nuevo (bares por ejemplo)
+//cargar fichero, modificar y guardar
+//https://stackabuse.com/reading-and-writing-json-files-with-node-js/ 
+
+
 var express = require('express') //llamamos a Express
 var app = express() 
 var port = 8080  // establecemos nuestro puerto
+let cervezasJson = require('./mode/cervezas.json');
 
 //middleware
 app.use(express.json());
-app.use('/cervezas', require('./routes/rutas.js'));
+app.use('/cervezas', require('./routes/cervezas.js'));
+app.use('/bares', require('./routes/bares.js'))
               
-let cervezas = [
-    {
-        "id": 1,
-        "nombre": "mahou",
-        "grados": 3.2
-    },
-    {
-        "id": 2,
-        "nombre": "cruzcampo",
-        "grados": 3.5
-    },
-    {
-        "id": 3,
-        "nombre": "ambar",
-        "grados": 4.2
-    }
-];
 
 // iniciamos nuestro servidor
 app.listen(port)
