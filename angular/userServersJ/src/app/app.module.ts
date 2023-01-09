3e3d863c002bf6578fcfe4b5626153559ba20e86
chisela14 <chisela14@gmail.com>
2023-01-09 10:26:57 +0100

    users servers v.joaquin

---
+++ angular/userServersJ/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsersModule } from './users/users.module';
+import { ServersModule } from './servers/servers.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    UsersModule,
+    ServersModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
